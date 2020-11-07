2928cdc5d9667a4027eaa5c6e0da6b30bb1276cb
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2020-11-06 17:17:52 -0800

    Update sctest.py

--- sctest.py
+++ sctest.py
@@ -1,7 +1,8 @@
-#Use this program to dial in the screen coordinates you need to make ddmd.py run properly
+#Use this program to dial in the screen coordinates you need to make ddmd.py run properly.
 #Run sctest.py with discord in fullscreen mode. Make note of where the mouse cursor moves
 #and edit the coordinates in this code as nessisary until the mouse cursor gos where you want it.
-#Once you have the correct coordinates, edit them into ddmd.py
+#Once you have the correct coordinates, use them when prompted by ddmd.py, or if you'd like them
+#to be the default edit them into ddmd.py
 
 #You will need to install the pyautogui library, which can be found here: 
 #https://pyautogui.readthedocs.io/en/latest/install.html
