5dd6ab1f234d4f61c88f82d31706e55132b87f30
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2020-11-07 11:44:14 -0800

    Update sctest.py

--- sctest.py
+++ sctest.py
@@ -1,8 +1,12 @@
-#Use this program to dial in the screen coordinates you need to make ddmd.py run properly.
-#Run sctest.py with discord in fullscreen mode. Make note of where the mouse cursor moves
-#and edit the coordinates in this code as nessisary until the mouse cursor gos where you want it.
-#Once you have the correct coordinates, use them when prompted by ddmd.py, or if you'd like them
-#to be the default edit them into ddmd.py
+#Use this program to dial in the screen coordinates you need to make ddmd.py run properly. 
+
+#Run sctest.py with the Discord desktop application in fullscreen mode or with the window
+#stretching top to bottom of your screen and moved all the way to the left. Make sure that
+#the upper left corner of the Discord application window is visible while other programs are
+#running. Take note of where the mouse cursor moves and edit the coordinates in this code as 
+#nessisary until the mouse cursor moves to the #Friends list tab and then the target conversation. 
+#Once you have the correct coordinates, use them when prompted by ddmd.py. If you'd like them to 
+#be the default, edit them into ddmd.py
 
 #You will need to install the pyautogui library, which can be found here: 
 #https://pyautogui.readthedocs.io/en/latest/install.html
