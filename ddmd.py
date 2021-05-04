cd8afec67656afe5e157163b4f0787799a84e704
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2021-05-04 16:42:10 -0700

    Update ddmd.py
    Added easier custom screen coordinate functionality

--- ddmd.py
+++ ddmd.py
@@ -1,6 +1,11 @@
 import pyautogui    #macro control library
+import sys
 import time
 from tqdm import tqdm   #progress bar library
+
+print('')
+print('DDMD is a python macro that automates mass deletion of Discord messages.')
+print('')
 
 #check user input for valid message deletion quota
 while True:
@@ -16,7 +21,7 @@ while True:
 
 #check user input for valid message edit mode setting
 while True:
-    editMode = input('Edit mode? y/n: ')
+    editMode = input('Edit messages before deletion? y/n: ')
     if len(editMode) < 1:
         editMode = 'n'
     elif editMode not in ('y', 'yes', 'Yes', 'n', 'no', 'No'):
@@ -34,56 +39,55 @@ while True:
 xa = 220    #Friends tab x default coordinate
 ya = 120    #Friends tab y default coordinate
 xb = 220    #conversation tab x default coordinate
-yb = 280    #conversation tab y default coordinate
+yb = 330    #conversation tab y default coordinate
+print('')
 print('Screen', pyautogui.size())   #print users screen resolution
 print('Default screen coordinates', (xa, xb),'and', (xb, yb))
 
 #checks user input for valid custom coordinate mode setting
 while True:
-    ccMode = input('Custom screen coordinates? y/n: ')
+    ccMode = input('Custom screen coordinates? (recommended for first time users) y/n: ')
     if len(ccMode) < 1:
-        ccMode = 'n'
+        ccMode = 'y'
     elif ccMode not in ('y', 'yes', 'Yes', 'n', 'no', 'No'):
         print('Invalid input: Please enter y or n')
         continue
     elif ccMode in ('n', 'no', 'No'):
         print('ccMode off')
         break
-    else:
+    if ccMode == 'y':
         print('ccMode on')
-        while True:    #starts loops asking for user to input screen coordinates
-            xa = input('Friends tab X coordinate: ')
+        xa = None
+        ya = None
+        xb = None
+        yb = None
+        counter = 0
+
+        print('')
+        print('Position cursor over the "Friends" tab in Discord and then press Ctrl-C.')
+        print('')
+
+        while xa == None or ya == None or xb == None or yb == None:
             try:
-                xa = int(xa)
-            except:
-                print('Invalid input:', xa, 'is not a number')
-                continue
-            while True:
-                ya = input('Friends tab Y coordinate: ')
-                try:
-                    ya = int(ya)
-                except:
-                    print('Invalid input:', ya, 'is not a number')
-                    continue
-                while True:
-                    xb = input('Conversation tab X coordinate: ')
-                    try:
-                        xb = int(xb)
-                    except:
-                        print('Invalid input:', xb, 'is not a number')
-                        continue
-                    while True:
-                        yb = input('Conversation tab Y coordinate: ')
-                        try:
-                            yb = int(yb)
-                        except:
-                            print('Invalid input:', yb, 'is not a number')
-                            continue
-                        break
-                    break
-                break
-            break
-        break
+                x, y = pyautogui.position()
+                positionStr = '(' + str(x) + ', ' + str(y) + ')'
+                print(positionStr, end='')
+                print('\b' * len(positionStr), end='')
+            except KeyboardInterrupt:
+                counter = counter + 1
+                if counter == 1:
+                    print('\b' * len(positionStr), end='')
+                    print('"Friends" tab coordinates are:', positionStr)
+                    xa = x
+                    ya = y
+                    print('')
+                    print('Next, position the cursor over the desired conversation and press Ctrl-C.')
+                if counter == 2:
+                    print('\b' * len(positionStr), end='')
+                    print('Conversation tab coordinates are:', positionStr)
+                    print('')
+                    xb = x
+                    yb = y
     break
 
 #logic gate for when edit mode is turned on
@@ -93,9 +97,21 @@ t = gateOpen ^ gateClosed
 gateStatus = gateOpen
 
 count = 0
-pyautogui.click(1, 1)   #moves mouse to upper left hand corner and selects app
+#pyautogui.click(1, 1)   #moves mouse to upper left hand corner and selects app
 
 #message deletion loop
+while True:
+    beginDeletion = input('Start deleting messages? y/n: ')
+    if len(beginDeletion) < 1:
+        pass
+    elif beginDeletion not in ('y', 'yes', 'Yes', 'n', 'no', 'No'):
+        print('Invalid input: Please enter y or n')
+        continue
+    elif beginDeletion in ('n', 'no', 'No'):
+        print('Exiting')
+        exit()
+    break
+
 for i in tqdm (range (iterations), desc="Deleting", ascii=False):
     pyautogui.click(xa, ya)   #moves mouse to Friends tab and clicks right
     pyautogui.click(xb, yb)   #moves mouse to conversation tab and clicks right
